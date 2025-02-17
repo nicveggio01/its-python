adbcce9e3ac812583f933a1f8550beb664639a44
nicveggio01 <nic.veggian@gmail.com>
2025-02-17 10:39:51 +0100

    updated

---
+++ lezione_01/esercizio5.10.py
@@ -0,0 +1,15 @@
+current_users:list=["stefano", "lisa", "sara", "max", "giorgio"] 
+new_users: list=["paola", "giovanni", "stefano", "sara", "francesca"] 
+
+for i in new_users:
+    if i in current_users:
+        print("the username is already used, please choose another one!")
+    
+else:
+    print("the username is available!)
+          
+current_users:list=["stefano", "lisa", "sara", "max", "giorgio"] 
+
+for i in current_users:
+    i.upper()
+    print("current_users")
