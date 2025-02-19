1a97b253b9c6dea1f1c716b54ec858a756922c58
nicveggio01 <nic.veggian@gmail.com>
2025-02-19 10:52:40 +0100

    updated

--- lezione_01/esercizio5.10.py
+++ lezione_01/esercizio5.10.py
@@ -8,8 +8,10 @@ for i in new_users:
 else:
     print("the username is not available")
           
-current_users:list=["stefano", "lisa", "sara", "max", "giorgio"] 
+current_users_upper =[user.upper() for user in current_users] 
 
-for i in current_users:
-    i.upper()
-    print("current_users")
+for user in new_users:
+    if user.upper() in current_users_upper:
+        print(f"Nome utente '{user}' già in uso, scegline uno diverso.")
+    else:
+        print(f"il nome utente '{user}' è già disponibile")
